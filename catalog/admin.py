6a57c977cb1eb9f7e5001ba3fa001d10f0b3f50a
Joanel Maravilla Divinagracia <joaneldivinagracia@gmail.com>
2013-03-25 20:20:56 +0800

    done with forms and models of catalog

---
+++ catalog/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from models import Product, Category
+from forms import ProductAdminForm
+
+
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductAdminForm
+
+    list_display = ('name', 'price', 'old_price', 'created_at', 'update_at',)
+    list_display_links = ('name',)
+    list_per_page = 50
+    ordering = ['-created_at']
+    search_fields = ['name', 'description']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at',)
+    list_display_links = ('name',)
+    list_per_page = 50
+    ordering = ['-created_at']
+    search_fields = ['name', 'description']
+
+
+admin.site.register(Category, CategoryAdmin)
---
+++ catalog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from models import Product
+
+
+class ProductAdminForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        exclude = ('created_at', 'update_at',)
+
+    def clean_price(self):
+        if self.cleaned_data['price'] <= 0:
+            raise forms.ValidationError('Price must be greater than zero')
+        return self.cleaned_data['price']
